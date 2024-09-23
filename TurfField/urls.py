f081d4e60b7eaab9878cdd0b50445efeac9f8930
Murat Furkan Sen <muratfurkan.sen79@gmail.com>
2024-09-23 15:35:52 +0300

    Apps Created and Linked

--- TurfField/urls.py
+++ TurfField/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-urlpatterns = [
+urlpatterns = ([
     path('admin/', admin.site.urls),
-]
+    path('', include('home.urls')),
+    path('home/', include('home.urls')),
+    path('user/', include('user.urls')),
+    path('team/', include('team.urls')),
+    path('reservation/', include('reservation.urls')),
+])
