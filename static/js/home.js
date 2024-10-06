43f748b2afaf13e96c2ee5341bf7a35f1f023432
Murat Furkan Sen <muratfurkan.sen79@gmail.com>
2024-10-06 16:25:18 +0300

    Reservation Button Fixed

--- static/js/home.js
+++ static/js/home.js
@@ -1,14 +1,8 @@
-var homeReservation = document.querySelector(".homeReservation");
-var closeSign = document.querySelector(".closeSign");
-var homeLog = document.querySelector(".homeLog");
-var closeLog = document.querySelector(".closeLog");
-var homeCreateTeam = document.querySelector(".takimOlusturButonu");
-var closeCreateTeam = document.querySelector(".closeCreate");
 let boolLog = false;
 let boolCreateTeam = false;
+let boolSign = false;
 
-
-homeLog.addEventListener("click", function() {
+function homeLog(){
     if (!boolCreateTeam){
         boolLog = true;
         document.querySelector(".login-container").style.display = "block";
@@ -20,27 +14,4 @@ homeLog.addEventListener("click", function() {
         document.querySelector(".login-container").style.display = "block";
     }
 
-})
-
-closeLog.addEventListener("click", function() {
-    boolLog = false;
-    document.querySelector(".login-container").style.display = "none";
-})
-
-homeCreateTeam.addEventListener("click", function() {
-    if (!boolLog){
-        boolCreateTeam = true;
-        document.querySelector(".create-team-button").style.display = "block";
-    }
-    else {
-        boolLog = false;
-        boolCreateTeam = true;
-        document.querySelector(".login-container").style.display = "none";
-        document.querySelector(".create-team-button").style.display = "block";
-    }
-})
-
-closeCreateTeam.addEventListener("click", function() {
-    boolCreateTeam = false;
-    document.querySelector(".create-team-button").style.display = "none";
-})
+}
