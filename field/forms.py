8723ec62176caee325ef8ade64fcfbf047f0244e
MuratFurkanSen <muratfurkan.sen79@gmail.com>
2024-11-08 18:49:43 +0300

    Field Creation Part1 Completed

--- field/forms.py
+++ field/forms.py
@@ -1,18 +1,85 @@
 from django import forms
 from .models import Field
 
+
 HOUR_CHOICES = tuple(
     (f'{str(i).zfill(2)}-{str(i + 1).zfill(2)}', f'{str(i).zfill(2)}-{str(i + 1).zfill(2)}') for i in range(24)
 )
+province_choices = (
+    ('1', 'Adana'),
+)
+district_choices = (
+    ('1','Seyhan'),
+)
+neighborhood_choices = (
+    ('1','Ahmet Remzi Yüreğir'),
+)
+street_choices = (
+    ('1','59004'),
+)
+building_choices = (
+    ('1','5'),
+)
+indoor_choices = (
+    ('1','1'),
+)
+
+
 class FieldCreationForm(forms.ModelForm):
     time_slots = forms.MultipleChoiceField(
         widget=forms.SelectMultiple,
         choices=HOUR_CHOICES,
         label='Time Slots',
     )
+    province_slots = forms.ChoiceField(
+        widget=forms.Select,
+        choices=province_choices,
+        label='Province Slots',
+    )
+    district_slots = forms.ChoiceField(
+        widget=forms.Select,
+        choices=district_choices,
+        label='District Slots',
+    )
+    neighborhood_slots = forms.ChoiceField(
+        widget=forms.Select,
+        choices=neighborhood_choices,
+        label='Neighborhood Slots',
+    )
+    street_slots = forms.ChoiceField(
+        widget=forms.Select,
+        choices=street_choices,
+        label='Street Slots',
+    )
+    building_slots = forms.ChoiceField(
+        widget=forms.Select,
+        choices=building_choices,
+        label='Building Slots',
+    )
+    indoor_slots = forms.ChoiceField(
+        widget=forms.Select,
+        choices=indoor_choices,
+        label='Indoor Slots',
+    )
     class Meta:
         model = Field
-        fields = "__all__"
-        widgets = {
+        fields = [# Field Info
+                  'name', 'time_slots', 'is_have_shoes',
+                  # Address Info
+                  'province_slots', 'district_slots', 'neighborhood_slots',
+                  'street_slots', 'building_slots', 'indoor_slots',
+                  'maps_location',]
+        widgets = {}
+    def save(self, commit=True):
+        field = super(FieldCreationForm, self).save(commit=False)
+        field.reservation_hours = ','.join(self.cleaned_data['time_slots'])
+        field.province = self.cleaned_data['province_slots']
+        field.district = self.cleaned_data['district_slots']
+        field.neighborhood = self.cleaned_data['neighborhood_slots']
+        field.street = self.cleaned_data['street_slots']
+        field.building = self.cleaned_data['building_slots']
+        field.indoor = self.cleaned_data['indoor_slots']
 
-        }
+        if commit:
+            field.save()
+        return field
--- field/models.py
+++ field/models.py
@@ -5,92 +5,21 @@ HOUR_CHOICES = tuple(
     (f'{str(i).zfill(2)}-{str(i+1).zfill(2)}', f'{str(i).zfill(2)}-{str(i+1).zfill(2)}') for i in range(24)
 )
 
-TURKEY_CITIES = (
-    ('empty', ''),
-    ('adana', 'Adana'),
-    ('adiyaman', 'Adıyaman'),
-    ('afyonkarahisar', 'Afyonkarahisar'),
-    ('agri', 'Ağrı'),
-    ('amasya', 'Amasya'),
-    ('ankara', 'Ankara'),
-    ('antalya', 'Antalya'),
-    ('artvin', 'Artvin'),
-    ('aydın', 'Aydın'),
-    ('balikesir', 'Balıkesir'),
-    ('bilecik', 'Bilecik'),
-    ('bingol', 'Bingöl'),
-    ('bitlis', 'Bitlis'),
-    ('bolu', 'Bolu'),
-    ('burdur', 'Burdur'),
-    ('bursa', 'Bursa'),
-    ('canakkale', 'Çanakkale'),
-    ('cankiri', 'Çankırı'),
-    ('corum', 'Çorum'),
-    ('denizli', 'Denizli'),
-    ('diyarbakir', 'Diyarbakır'),
-    ('edirne', 'Edirne'),
-    ('elazig', 'Elazığ'),
-    ('erzincan', 'Erzincan'),
-    ('erzurum', 'Erzurum'),
-    ('eskisehir', 'Eskişehir'),
-    ('gaziantep', 'Gaziantep'),
-    ('giresun', 'Giresun'),
-    ('gumushane', 'Gümüşhane'),
-    ('hakkari', 'Hakkari'),
-    ('hatay', 'Hatay'),
-    ('igdir', 'Iğdır'),
-    ('isparta', 'Isparta'),
-    ('istanbul', 'İstanbul'),
-    ('izmir', 'İzmir'),
-    ('kahramanmaras', 'Kahramanmaraş'),
-    ('karabuk', 'Karabük'),
-    ('karaman', 'Karaman'),
-    ('kars', 'Kars'),
-    ('kastamonu', 'Kastamonu'),
-    ('kayseri', 'Kayseri'),
-    ('kirikkale', 'Kırıkkale'),
-    ('kirklareli', 'Kırklareli'),
-    ('kirsehir', 'Kırşehir'),
-    ('kocaeli', 'Kocaeli'),
-    ('konya', 'Konya'),
-    ('kutahya', 'Kütahya'),
-    ('malatya', 'Malatya'),
-    ('manisa', 'Manisa'),
-    ('mardin', 'Mardin'),
-    ('mersin', 'Mersin'),
-    ('mugla', 'Muğla'),
-    ('mus', 'Muş'),
-    ('nevsehir', 'Nevşehir'),
-    ('nigde', 'Niğde'),
-    ('ordu', 'Ordu'),
-    ('osmaniye', 'Osmaniye'),
-    ('rize', 'Rize'),
-    ('sakarya', 'Sakarya'),
-    ('samsun', 'Samsun'),
-    ('siirt', 'Siirt'),
-    ('sinop', 'Sinop'),
-    ('sivas', 'Sivas'),
-    ('sanliurfa', 'Şanlıurfa'),
-    ('sirnak', 'Şırnak'),
-    ('tekirdag', 'Tekirdağ'),
-    ('tokat', 'Tokat'),
-    ('trabzon', 'Trabzon'),
-    ('tunceli', 'Tunceli'),
-    ('usak', 'Uşak'),
-    ('van', 'Van'),
-    ('yalova', 'Yalova'),
-    ('yozgat', 'Yozgat'),
-    ('zonguldak', 'Zonguldak'),
-)
 
 class Field(models.Model):
+    # Field Related Info
     name = models.CharField(max_length=100)
-    city = models.CharField(max_length=100, choices=TURKEY_CITIES, default="empty")
-    district = models.CharField(max_length=100)
-    open_address = models.CharField(max_length=100)
-    maps_location = models.URLField()
-    reservation_hours = models.CharField(max_length=100, choices=HOUR_CHOICES) # "Starting Hours" 9,10,11
+    reservation_hours = models.CharField(max_length=255, choices=HOUR_CHOICES)  # "Starting Hours" 9,10,11
     is_have_shoes = models.BooleanField()
+
+    # Address Related Info
+    province = models.CharField(max_length=100)
+    district = models.CharField(max_length=100)
+    neighborhood = models.CharField(max_length=100)
+    street = models.CharField(max_length=100)
+    building = models.CharField(max_length=100)
+    indoor = models.CharField(max_length=100)
+    maps_location = models.URLField()
 
     def __str__(self):
         return f"{self.id} - {self.name}"
