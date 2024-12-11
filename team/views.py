03edcc528fa9b4a1b36eac8a3f7ace851582f295
MuratFurkanSen <muratfurkan.sen79@gmail.com>
2024-12-11 18:34:40 +0300

    Team Edit Page Updated

--- team/views.py
+++ team/views.py
@@ -2,9 +2,9 @@ from django.contrib.auth.models import User
 from django.http import HttpResponseForbidden
 from django.shortcuts import render, redirect
 
-from .apps import TeamConfig
 from .forms import TeamCreationForm
 from .models import Team
+
 
 # Create your views here.
 def create_team(request):
@@ -18,34 +18,31 @@ def create_team(request):
             return redirect(f"/team/{team.id}")
 
 
-def edit_team(request, id, op):
-    team = Team.objects.get(id=id)
+def edit_team(request, team_id, op, user_id):
+    team = Team.objects.get(id=team_id)
     if request.user != team.captain:
         return HttpResponseForbidden("You are not allowed to edit this team")
-    if request.method == 'POST':
-        if op == "add":
-            user = User.objects.get(id=request.POST.get('id'))
-            team.members.add(user)
-            team.save()
-            return redirect(f"/team/{id}")
-        elif op == "remove":
-            user = User.objects.get(id=request.POST.get('id'))
-            team.members.remove(user)
-            team.save()
-            if len(team.members.all()) == 0:
-                team.delete()
-                return redirect(f"/")
-            return redirect(f"/team/{id}")
-        elif op == 'delete':
+    if op == "add":
+        user = User.objects.get(id=user_id)
+        team.members.add(user)
+        team.save()
+        return redirect(f"/team/{team_id}")
+    elif op == "remove":
+        user = User.objects.get(id=user_id)
+        team.members.remove(user)
+        team.save()
+        if len(team.members.all()) == 0:
             team.delete()
-            return redirect("/")
+            return redirect(f"/")
+        return redirect(f"/team/{team_id}")
+    elif op == 'delete':
+        team.delete()
+        return redirect("/")
 
 
-
-def team(request, id):
-    teams = Team.objects.get(id=id)
-    if request.user not in teams.members.all():
+def team(request, team_id):
+    team = Team.objects.get(id=team_id)
+    if request.user not in team.members.all():
         return HttpResponseForbidden("You are not a member of this team")
-    create_form = TeamCreationForm()
-    context = {'create_form': create_form, 'id': id, 'teams': teams}
+    context = {'id': team_id, 'team': team}
     return render(request, 'team.html', context)
